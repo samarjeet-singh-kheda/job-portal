1341563c41f524afc9b5ad4515cb5b8f4f55fcbf
samarjeet-singh-kheda <samarjeetsingh259@gmail.com>
2024-09-08 01:22:29 +0530

    Added tables in supabase DB with policies, implemennted job fetching via custom hook & job listings page

---
+++ src/components/JobCard.jsx
@@ -0,0 +1,62 @@
+import { useUser } from "@clerk/clerk-react";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Heart, MapPinIcon, Trash2Icon } from "lucide-react";
+import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+
+function JobCard({
+  job,
+  isMyJob = false,
+  savedInit = false,
+  onJobSaved = () => {},
+}) {
+  const { user } = useUser();
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>
+          {job.title}
+
+          {isMyJob && (
+            <Trash2Icon
+              fill="red"
+              size={18}
+              className="text-red-300 cursor-pointer"
+            />
+          )}
+        </CardTitle>
+      </CardHeader>
+
+      <CardContent className="flex flex-col gap-4 flex-1">
+        <div className="flex justify-between">
+          {job.company && <img src={job.company.logo_url} className="h-6" />}
+
+          <div className="flex gap-2 items-center">
+            <MapPinIcon size={15} /> {job.location}
+          </div>
+        </div>
+        <hr />
+        {job.description.substring(0, job.description.indexOf("."))}
+      </CardContent>
+
+      <CardFooter className="flex gap-2">
+        <Link to={`/job/${job.id}`} className="flex-1">
+          <Button variant="secondary" className="w-full">
+            More Details
+          </Button>
+        </Link>
+
+        <Heart size={20} stroke="red" fill="red" />
+      </CardFooter>
+    </Card>
+  );
+}
+
+export default JobCard;
--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -1,5 +1,54 @@
+import { getJobs } from "@/api/apiJobs";
+import JobCard from "@/components/JobCard";
+import useFetch from "@/hooks/useFetch";
+import { useUser } from "@clerk/clerk-react";
+import { useEffect, useState } from "react";
+import { BarLoader } from "react-spinners";
+
 function JobListing() {
-  return <>JobListing</>;
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState("");
+  const [company_id, setCompany_id] = useState("");
+
+  const { isLoaded } = useUser();
+
+  const {
+    fn: fnJobs,
+    data: jobs,
+    loading: loadingJobs,
+  } = useFetch(getJobs, { location, company_id, searchQuery });
+
+  useEffect(() => {
+    isLoaded && fnJobs();
+  }, [isLoaded, location, company_id, searchQuery]);
+
+  if (!isLoaded) {
+    return <BarLoader className="mb-4" width={"100%"} color="#2d2696" />;
+  }
+
+  return (
+    <div>
+      <h1 className="gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8">
+        Latest Jobs
+      </h1>
+
+      {/* Add filters here */}
+
+      {loadingJobs && (
+        <BarLoader className="mt-4" width={"100%"} color="#2d2696" />
+      )}
+
+      {loadingJobs === false && (
+        <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {jobs?.length ? (
+            jobs.map((job) => <JobCard key={job.id} job={job} />)
+          ) : (
+            <div>No Jobs Found</div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default JobListing;
